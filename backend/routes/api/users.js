336afd0ddf5ac7778ae446686801561094de92af
William <wcathey1604@gmail.com>
2024-11-01 17:41:01 -0500

    setup authorization for user and session

---
+++ backend/routes/api/users.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const {validateSignup} = require('../../utils/validation')
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+
+const router = express.Router();
+
+  router.post(
+    '/',
+    validateSignup,
+    async (req, res) => {
+      const {firstName, lastName, email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
+
+      const safeUser = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+      };
+
+      await setTokenCookie(res, safeUser);
+      res.status(201)
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
+module.exports = router;
---
+++ backend/utils/validation.js
@@ -0,0 +1,52 @@
+const { validationResult } = require('express-validator');
+const { check } = require('express-validator');
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach(error => errors[error.path] = error.msg);
+
+    const err = Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+  next();
+};
+
+const validateSignup = [
+    check('firstName')
+      .exists({checkFalsy: true})
+      .withMessage("First Name is required"),
+    check('lastName')
+      .exists({checkFalsy: true})
+      .withMessage("Last Name is required"),
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+  module.exports = {
+    handleValidationErrors, validateSignup
+  }
